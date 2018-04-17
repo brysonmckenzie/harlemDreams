2a700d81146220fad0025d54279898b91b23e337
brysonmckenzie <brysonmckenzie83@gmail.com>
2018-04-17 10:25:35 -0500

    pushig worked files up

--- Harlem_Dreams/settings.py
+++ Harlem_Dreams/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -117,5 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+
+
+
+# STATIC_ROOT = "app-root/Harlem_Dreams/wsgi/static"
+
+STATICFILES_DIR = (os.path.join(BASE_DIR, "/dream_app/static"))
 
 STATIC_URL = '/static/'
--- apps/dream_app/views.py
+++ apps/dream_app/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render, redirect, HttpResponse
 
 
 def index(request):
-
+    
     return render(request, 'dream_app/index.html')
+
+def page2(request):
+
+    return render(request, 'dream_app/index2.html')

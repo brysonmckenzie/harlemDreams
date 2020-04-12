8cef3200239fe8d5608cb04ea7ccff5f4bec9301
brysonmckenzie <brysonmckenzie83@gmail.com>
2020-04-12 06:39:00 -0700

    prod_update

--- Harlem_Dreams/settings.py
+++ Harlem_Dreams/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'Harlem_Dreams.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 DATABASES = {
      'default': {

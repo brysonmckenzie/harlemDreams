996dc3f16fb0bf3cfe26c8f6abf61843be5d7cbd
brysonmckenzie <brysonmckenzie83@gmail.com>
2020-04-13 08:00:01 -0700

    update

--- Harlem_Dreams/urls.py
+++ Harlem_Dreams/urls.py
@@ -17,10 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.dream_app.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
--- apps/dream_app/urls.py
+++ apps/dream_app/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     url(r'^process$', views.process_contact),
     url(r'^sponsors$', views.sponsors),
     url(r'^founders$', views.founder),
-    url(r'^media$', views.media),
+    url(r'^dreamsmedia$', views.harlemedia),
 ]
